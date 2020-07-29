2b13da8b60dea77a3cd0dfe18cc87b55223ebcd1
Rogerio Pelizari <rogpelizari@hotmail.com>
2020-07-28 21:43:01 -0300

    Ajustes formulario

--- script.php
+++ script.php
@@ -9,6 +9,28 @@ $categorias[] = 'idoso';
 $nome = $_POST['nome'] ;
 $idade = $_POST['idade'];
 
+if(empty($nome))
+{
+    echo "O nome não pode ser vazio";
+    return;
+}
+if(strlen($nome) < 3 )
+{
+    echo  " O nome deve conter 3 ou mais caracteres";
+    return;
+}
+if(strlen($nome) > 20)
+{
+    echo  " O nome é muito extenso";
+    return;
+}
+if(!is_numeric($idade))
+{
+    echo "Informe um número para idade";
+    return;
+}
+
+
 if($idade >= 6 && $idade <= 12) 
 {
     for($i = 0; $i < count($categorias); $i++)
